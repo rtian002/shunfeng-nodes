58097228b7d7bb482a3fc99c33809de2e84ce1a3
rtian002 <rtian002@gmail.com>
2025-09-09 10:12:26 +0800

    Update sf.sh

--- sf.sh
+++ sf.sh
@@ -2,7 +2,9 @@
 url1='https://skill-note.blogspot.com/search/label/Blogger'
 curl -o shunfeng1.html $url1
 sleep 5
+ls
 url2=$(python3 main.py shunfeng1.html)
 curl -o shunfeng2.html $url2
 sleep 5
+ls
 python3 main.py shunfeng2.html
